cf5ea0362c4d072e23f536fe13280d6361741761
yohanesarmando2007-cloud <yohanesarmando2007@gmail.com>
2026-03-09 10:35:56 +0700

    P2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
     selamat datang di tokopeddia {{$nama}}, tempat dimana menjual produk produk secara online <br>
     anda adalah seorang {{$pekerjaan}}
     <!-- An unexamined life is not worth living. - Socrates -->
 </div>
+</body>
+</html>
